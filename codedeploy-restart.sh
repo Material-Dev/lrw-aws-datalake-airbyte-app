94f60fc561ad816fe180912e3c577230763a8cd7
Ara Gevorkian <ara.gevorkian@materialplus.io>
2023-02-22 20:25:45 -0800

    Set basic auth values from secrets manager

--- codedeploy-restart.sh
+++ codedeploy-restart.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd /home/ec2-user/airbyte
-/usr/local/bin/docker-compose -f docker-compose.yaml down
+/usr/local/bin/docker-compose -f docker-compose.yaml stop
+
+# Set basic auth from secrets manager
+username=$(aws secretsmanager get-secret-value --region us-west-2 --query SecretString --output text --secret-id $AIRBYTE_ENVIRONMENT/airbyte-basic-auth | jq .username | tr -d '"')
+password=$(aws secretsmanager get-secret-value --region us-west-2 --query SecretString --output text --secret-id $AIRBYTE_ENVIRONMENT/airbyte-basic-auth | jq .password | tr -d '"')
+
+sed -i "s/BASIC_AUTH_USERNAME=.*/BASIC_AUTH_USERNAME=$username/g" .env
+sed -i "s/BASIC_AUTH_PASSWORD=.*/BASIC_AUTH_PASSWORD=$password/g" .env
+
 /usr/local/bin/docker-compose -f docker-compose.yaml up -d
